2c4d3a015362ff1648b9b8cba4f671e4e0a89aa3
booz24443 <booz2449@gmail.com>
2019-09-12 18:50:26 +0430

    Working on shit _2

---
+++ src/components/pages/Members.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import SchoolMembers from "../sections/SchoolMembers";
+
+class Members extends Component {
+    render() {
+        return (
+            <div>
+                <SchoolMembers/>
+            </div>
+        );
+    }
+}
+
+export default Members;
